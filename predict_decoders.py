1e3f0c34a5298f78cf29b0196803fea7927a1c82
JeanKusanagi <jean@hotmail.com>
2025-12-31 21:17:34 -0300

    update

--- predict_decoders.py
+++ predict_decoders.py
@@ -225,9 +225,6 @@ class LoberModule(pl.LightningModule):
 
 			image = image.squeeze().numpy()
 
-			#image = mask_to_onehot(image)
-			#image = torch.from_numpy(image).unsqueeze(dim=0).float()
-
 			image = mask_to_onehot(image)
 			image = np.expand_dims(image, 0)
 
